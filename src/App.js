94a3c03f0ff46292d7fd7fc226e6a5f3dfed43e8
jose nieva <joseluisnievavillogas@gmail.com>
2023-06-27 21:41:36 -0500

    ruta pdf

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/pdfElement" element={<PDFElement />} />
+          <Route path="/pdf" element={<PDFElement />} />
         </Routes>
       </Router>
     </div>
